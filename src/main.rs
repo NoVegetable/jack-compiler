aa35b7599921fb0dc27db2b46c83381f4bbd6539
NoVegetable <jinkey0202@gmail.com>
2025-12-22 16:31:26 +0800

    Complete main function
    We finished the main function implementation. We now support two output
    formats: XML and debug printing. The former is used by the test cases,
    and the latter is Rust debug formatting with pretty-print, which shows
    the whole structure and data of the built AST. We also support writing
    the output to a specified file instead of just stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,84 @@
-use jack_compiler::lexer;
-use jack_compiler::parser;
-use std::env;
+use clap::{Arg, Command};
+use jack_compiler::utils::XmlWrite;
+use jack_compiler::{lexer, parser, utils};
+use std::ffi::OsStr;
 use std::fs;
 use std::io;
+use std::io::Write;
+use std::path::Path;
 
 fn main() -> io::Result<()> {
-    let args = env::args().collect::<Vec<_>>();
+    let matches = Command::new("jack-compiler")
+        .arg(Arg::new("output").short('o').long("output").help(
+            "The output path for the generated AST. If not set, the output would be set to stdout.",
+        ))
+        .arg(
+            Arg::new("format")
+                .short('f')
+                .long("format")
+                .value_parser(["xml", "debug"])
+                .default_value("xml")
+                .long_help(
+"The output format. Possible values are: 'xml', 'debug'. They correspond to the 2 formats we
+supports: XML and Rust debug print. The default value is 'xml'"
+                ),
+        )
+        .arg(
+            Arg::new("input")
+                .help("The input Jack source file.")
+                .required(true)
+        )
+        .get_matches();
 
-    if args.len() < 2 {
-        panic!("Please provide Jack source file(s).\n");
+    let input = matches.get_one::<String>("input").unwrap();
+    let output = matches.get_one::<String>("output");
+    let format = matches.get_one::<String>("format").unwrap();
+
+    let source = fs::read_to_string(input)?;
+
+    let lexer = lexer::Lexer::new(&source);
+    let parser = parser::ClassParser::new();
+    let ast = parser
+        .parse(lexer)
+        .unwrap_or_else(|e| panic!("error occurs while parsing: {:?}", e));
+
+    if let Some(out_path) = output {
+        ensure_parent(out_path)?;
+
+        let mut out = fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(out_path)?;
+
+        if format == "xml" {
+            let mut writer = utils::init_writer(out);
+            if let Err(e) = ast.write_xml(&mut writer) {
+                panic!("error occurs while writing output: {}", e);
+            }
+        } else {
+            out.write(format!("{:#?}", ast).as_bytes())?;
+        }
+    } else {
+        let out = io::stdout();
+        if format == "xml" {
+            let mut writer = utils::init_writer(out);
+            if let Err(e) = ast.write_xml(&mut writer) {
+                panic!("error occurs while writing output: {}", e);
+            }
+        } else {
+            print!("{:#?}", ast);
+        }
     }
 
-    for arg in &args[1..] {
-        let source = fs::read_to_string(arg)?;
-        let lexer = lexer::Lexer::new(&source);
-        let parser = parser::ClassParser::new();
-        let ast = parser
-            .parse(lexer)
-            .unwrap_or_else(|e| panic!("error occurs while parsing: {:?}", e));
-        println!("{:#?}", ast);
-    }
+    Ok(())
+}
 
+fn ensure_parent<S: AsRef<OsStr> + ?Sized>(s: &S) -> io::Result<()> {
+    let path = Path::new(s);
+    let Some(parent) = path.parent() else {
+        panic!("invalid output path");
+    };
+    fs::create_dir_all(parent)?;
     Ok(())
 }
